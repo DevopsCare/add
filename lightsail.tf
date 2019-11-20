12066b7739779c2184cb2e4f615caaeb5bafd482
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2019-11-20 02:01:55 +0200

    Create lightsail.tf

---
+++ lightsail.tf
@@ -0,0 +1,4 @@
+resource "aws_lightsail_key_pair" "pveretennikovs" {
+  name       = "PVeretennikovs"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAING4k9RcHggUG2Gwr4+4/H02+oNDol8aFmD4xtnRO7HE ce:9f:1a:d6:53:1c:28:8c:40:47:b7:93:e2:3c:2c:e5 PVeretennikovs@2019"
+}
