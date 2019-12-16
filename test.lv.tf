1cd20498997ff60d1fc5910fcff69a8a7de50ecf
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2019-12-16 18:03:00 +0300

    add test.lv

---
+++ test.lv.tf
@@ -0,0 +1,27 @@
+module "dmitrijsv" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
+  namespace   = "dmitrijsv"
+  environment = "test.lv"
+}
+
+resource "cloudflare_zone" "test_lv" {
+  zone = "test.lv"
+  plan = "free"
+  type = "full"
+}
+
+resource "cloudflare_record" "test_lv" {
+  zone_id = cloudflare_zone.test_lv.zone
+  name    = module.ennoiart.environment
+  value   = aws_lightsail_static_ip.wordpress.ip_address
+  type    = "A"
+  ttl     = 86400
+}
+
+resource "cloudflare_record" "www_ennoiart_com" {
+  zone_id = cloudflare_zone.test_lv.zone
+  name    = "www.${module.ennoiart.environment}"
+  value   = aws_lightsail_static_ip.wordpress.ip_address
+  type    = "A"
+  ttl     = 86400
+}
