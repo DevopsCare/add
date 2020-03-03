412dda0b05021400524f2edc6fd40de633e19f45
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-03-03 22:33:33 +0200

    fix: try zone first

--- ennoiart.com.tf
+++ ennoiart.com.tf
@@ -9,6 +9,7 @@ resource "cloudflare_zone" "ennoiart_com" {
   plan = "free"
   type = "full"
 }
+/*
 
 resource "cloudflare_record" "ennoiart_com" {
   zone_id = cloudflare_zone.ennoiart_com.id
@@ -27,6 +28,7 @@ resource "cloudflare_record" "www_ennoiart_com" {
   ttl     = 1
   proxied = true
 }
+*/
 
 output "ennoiart_com" {
   value = cloudflare_zone.ennoiart_com.name_servers
